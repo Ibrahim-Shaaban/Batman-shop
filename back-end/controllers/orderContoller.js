c03d35e95320867fc4308343721aedfa3bfc9b79
Ibrahim-Shaaban <hemaabdelnaby325@gmail.com>
2020-10-19 02:21:35 +0200

    finish get order route

--- back-end/controllers/orderContoller.js
+++ back-end/controllers/orderContoller.js
@@ -30,4 +30,15 @@ const createOrder = asyncHandler(async (req, res) => {
   }
 });
 
-export { createOrder };
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (order) return res.json(order);
+  res.status(404);
+  throw new Error("order is not found");
+});
+
+export { createOrder, getOrderById };
--- back-end/routes/orderRoutes.js
+++ back-end/routes/orderRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 const router = express.Router();
 
-import { createOrder } from "../controllers/orderContoller.js";
+import { createOrder, getOrderById } from "../controllers/orderContoller.js";
 import { protect } from "../middlewares/authMiddleware.js";
 
 // private
 router.route("/").post(protect, createOrder);
+router.route("/:id").get(protect, getOrderById);
 
 export default router;
