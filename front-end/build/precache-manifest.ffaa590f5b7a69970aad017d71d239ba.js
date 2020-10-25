7909e48d956aaca148a2cdb72b463402b01890f0
Ibrahim-Shaaban <hemaabdelnaby325@gmail.com>
2020-10-25 13:18:50 +0200

    prepare for deployment

---
+++ front-end/build/precache-manifest.ffaa590f5b7a69970aad017d71d239ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dee6db363f8ebcd9dad9228ec5f2749a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51df608dddb4c000c8a3",
+    "url": "/static/css/main.8216a0f2.chunk.css"
+  },
+  {
+    "revision": "a7d2d492f8ff7a60e954",
+    "url": "/static/js/2.8fceed4f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8fceed4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51df608dddb4c000c8a3",
+    "url": "/static/js/main.666effdf.chunk.js"
+  },
+  {
+    "revision": "59728393beedaf83f2a7",
+    "url": "/static/js/runtime-main.f6c33f01.js"
+  }
+]);
